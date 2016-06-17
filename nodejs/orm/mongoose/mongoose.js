bdc2200d4dd6f835abafa40f9aa85a3269623152
L <my917@qq.com>
2016-06-17 17:45:27 +0800

    调整目录结构，mongoose demo

---
+++ nodejs/orm/mongoose/mongoose.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test_mongoose');
+
+console.log(1);
+
+var Schema = mongoose.Schema;
+
+var blogSchema = new Schema({
+    title:  String,
+    author: { type: String, default: 'hahaha' },
+    body: { type: [[{
+        bd:{type: String, default: 'hahaha'}
+    }]],
+        default: [[{
+            bd:11
+        }],[{
+            bd:22
+        }],[{
+        }]] },
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+        votes: Number,
+        favs:  Number
+    }
+});
+
+blogSchema.methods.findSimilarTitles = function (cb) {
+    return this.model('Blog').find({ title: this.title }, cb);
+};
+
+var Blog = mongoose.model('Blog', blogSchema);
+
+var oneBlog  = new Blog({title:11});
+
+// oneBlog.findSimilarTitles(function (err, dogs) {
+//     console.log(dogs.length); // woof
+//     console.log(dogs); // woof
+// });
+
+oneBlog.save();
