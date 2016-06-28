2ef49b142f1f1742919b3847b093a75863c726dc
L <my917@qq.com>
2016-06-28 15:12:16 +0800

    add promise lession

---
+++ nodejs/promise/promise.js
@@ -0,0 +1,37 @@
+var Q = require("q");
+var fun1 = function (data) {
+    var deferred = Q.defer();
+    deferred.resolve(data+" fun1");
+    return deferred.promise;
+};
+
+var fun2 = function (data) {
+    var deferred = Q.defer();
+    deferred.resolve(data+" fun2","data2");
+    return deferred.promise;
+};
+
+var fun3 = function (data,data2) {
+    console.log(data);
+    console.log(data2);
+    var deferred = Q.defer();
+    for(var i=0;i<3;i++){
+        deferred.resolve(data + " fun3" + i);
+    }
+    return deferred.promise;
+};
+
+function main(data,cb){
+    fun1("test")
+        .then(fun2)
+        .then(fun3)
+        .done(function(data){
+            cb(null,data);//ok 获得的最终数据为 --->"test fun1 fun2 fun3"
+        },function(err){
+            cb(err);//failed
+        });
+}
+
+main("haha",function (err,data) {
+    console.log(data);
+});
