070be9e7b8046dcf9bebf51e29d2b6d0e9a70ad4
L <my917@qq.com>
2016-06-29 19:48:29 +0800

    ride 重写遍历生成文件模块，可以获得完成状态

--- web_ride/app/controller/api/actions.js
+++ web_ride/app/controller/api/actions.js
@@ -40,9 +40,11 @@ actions.createProjectFiles = function (req, res) {
             };
             
             var createFiles = function (data) {
-                console.log("createFiles");
+                // console.log("createFiles");
                 var deferred = Q.defer();
                 if( data.node.fileType == "dir" ) {
+                    data.length.length --;
+                    console.log(data.length.length);
                     var dir = data.path + "\\" + data.node.name;
                     var nData = {
                         path: dir,
@@ -53,47 +55,155 @@ actions.createProjectFiles = function (req, res) {
                             console.log(err);
                             return;
                         }
-                        fs.writeFile(nData.path + '/__init__.txt', "__init__", {flag: 'w'}, function (err) {
+                        console.log(dir);
+                        var initFileFullName = nData.path + '/__init__.txt';
+                        fs.writeFile(initFileFullName, "__init__", {flag: 'w'}, function (err) {
                             if(err) {
                                 console.error(err);
                             } else {
-                                console.log('写入成功');
+                                console.log(initFileFullName);
                                 deferred.resolve(nData);
                             }
                         });
-
                     });
                 } else if (data.node.fileType == "file") {
+                    data.length.length --;
+                    console.log(data.length.length);
                     var fileFullName = data.path + '/' + data.node.name + '.txt';
                     fs.writeFile(fileFullName, "file", {flag: 'w'}, function (err) {
                         if (err) {
                             console.error(err);
                         } else {
-                            console.log('写入成功');
+                            console.log(fileFullName);
                         }
                     });
-
                 }
                 return deferred.promise;
             };
 
             var createChildren = function (data) {
+                var deferred = Q.defer();
                 data.node.children(function (err,children) {
+                    var length = {};
+                    length.length = children.length;
                     children.forEach(function (child) {
                         var nData = {
                             path: data.path,
-                            node:child
+                            node:child,
+                            length:length
                         };
                         createFiles(nData)
                             .then(createChildren);
                     })
                 });
+                return deferred.promise;
+            };
+
+            var createProject = function (data) {
+                var deferred = Q.defer();
+                var path = data.path + "/" +  data.node.name;
+                fs.mkdir(path, 0666, function (err) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log(path);
+                    var initFileFullName = path + '/__init__.txt';
+                    fs.writeFile(initFileFullName, "__init__", {flag: 'w'}, function (err) {
+                        if(err) {
+                            console.error(err);
+                        } else {
+                            console.log(initFileFullName);
+                        }
+                        data.node.children(function (err,children) {
+                            var nData = {
+                                path: path,
+                                nodes:children
+                            };
+                            deferred.resolve(nData);
+                        });
+                    });
+                });
+                return deferred.promise;
+            };
+            var lev = 0;
+            var walk = function (data) {
+                console.log("walk");
+                var deferred = Q.defer();
+                var len = data.nodes.length;
+                console.log(lev + ":"+ len);
+                lev ++;
+                if(len == 0){
+                    deferred.resolve();
+                }
+
+                data.nodes.forEach(function (child) {
+                    if(child.fileType == "dir"){
+                        //创建目录
+                        var path = data.path + "/" + child.name;
+                        fs.mkdir(path, 0666, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            console.log(path);
+                            //创建__init__文件
+                            var initFileFullName = path + '/__init__.txt';
+                            fs.writeFile(initFileFullName, "__init__", {flag: 'w'}, function (err) {
+                                if(err) {
+                                    console.error(err);
+                                } else {
+                                    console.log(initFileFullName);
+                                    //获取子元素
+                                    child.children(function (err,children) {
+                                        var nData = {
+                                            path: path,
+                                            nodes:children
+                                        };
+                                        walk(nData)
+                                            .then(function () {
+                                                len --;
+                                                console.log(len);
+                                                if(len == 0){
+                                                    deferred.resolve();
+                                                }
+                                            });
+                                    });
+                                }
+                            });
+                        });
+                    }else if(child.fileType == "file"){
+                        len --;
+                        console.log(len);
+                        if(len == 0){
+                            deferred.resolve();
+                        }
+                    }else{
+                        len --;
+                        console.log(len);
+                        if(len == 0){
+                            deferred.resolve();
+                        }
+                    }
+                });
+                return deferred.promise;
             };
 
             removeOldFiles()
                 .then(createRootDir)
-                .then(createFiles)
-                .then(createChildren);
+                .then(createProject)
+                .then(walk)
+                .then(function () {
+                    console.log("finish");
+                });
+
+            // removeOldFiles()
+            //     .then(createRootDir)
+            //     .then(createFiles)
+            //     .then(createChildren)
+            //     .then(function (data) {
+            //         console.log("finish")
+            //     });
 
             res.json(res.resFormat);
         } else {
