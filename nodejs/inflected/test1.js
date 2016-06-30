63c11301018ea2f25637ac973ace9d222655f197
L <my917@qq.com>
2016-06-30 15:13:34 +0800

    update

--- nodejs/inflected/test1.js
+++ nodejs/inflected/test1.js
@@ -1,3 +1,5 @@
 var Inflector = require('inflected');
 
 console.log(Inflector.pluralize('Category') ); // => 'Categories'
+console.log(Inflector.classify('poSt') );
+console.log(Inflector.camelize('fooBar') );
