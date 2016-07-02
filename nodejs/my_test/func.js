ec9838889d9fff9b54a1352b6bb2757fe9a2a6f7
L <my917@qq.com>
2016-07-02 20:08:24 +0800

    update

---
+++ nodejs/my_test/func.js
@@ -0,0 +1,28 @@
+function Foo() {
+    getName = function () {
+        console.log(1);
+    };
+    return this;
+}
+
+Foo.getName = function () {
+    console.log(2);
+};
+Foo.prototype.getName = function () {
+    console.log(3);
+};
+var getName = function () {
+    console.log(4);
+};
+function getName() {
+    console.log(5);
+}
+
+//node环境和浏览器环境执行效果不同
+// Foo.getName();
+// getName();
+// Foo().getName();
+// getName();
+// new Foo.getName();
+// new Foo().getName();
+// new new Foo().getName();
