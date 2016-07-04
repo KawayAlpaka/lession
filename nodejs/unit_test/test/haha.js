208189d85ccbd848ea4b83879daf71fe0e8d68eb
L <my917@qq.com>
2016-07-04 14:37:50 +0800

    add nodejs unit test lession

---
+++ nodejs/unit_test/test/haha.js
@@ -0,0 +1,29 @@
+require("should");
+
+var name = "zhaojian";
+
+describe("Name", function() {
+    it("The name should be zhaojian", function() {
+        name.should.eql("zhaojian");
+    });
+});
+
+var Person = function(name) {
+    this.name = name;
+};
+var zhaojian = new Person(name);
+
+describe("InstanceOf", function() {
+    it("Zhaojian should be an instance of Person", function() {
+        zhaojian.should.be.an.instanceof(Person);
+    });
+
+    it("Zhaojian should be an instance of Object", function() {
+        zhaojian.should.be.an.instanceof(Object);
+    });
+});
+describe("Property", function() {
+    it("Zhaojian should have property name", function() {
+        zhaojian.should.have.property("name");
+    });
+});
