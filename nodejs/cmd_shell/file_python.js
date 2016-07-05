a2b590403ff6855c3625b7292529b15b5d0640e6
L <my917@qq.com>
2016-07-05 19:35:10 +0800

    update

--- nodejs/cmd_shell/file_python.js
+++ nodejs/cmd_shell/file_python.js
@@ -1,8 +1,9 @@
 var exec = require('child_process').exec;
 var arg1 = 'hello';
 var arg2 = 'jzhou';
-exec('python py_test.py '+ arg1+' '+arg2+' ',function(error,stdout,stderr){
+// exec('python py_test.py '+ arg1+' '+arg2+' ',function(error,stdout,stderr){
 // exec('pybot D:\\test\\5774802151a57da02438381c\\哈哈2',function(error,stdout,stderr){
+exec('pybot D:/test/5774802151a57da02438381c/哈哈2',function(error,stdout,stderr){
     if(stdout.length >1){
         console.log('you offer args:',stdout);
     } else {
