36324c421a7b794f9ec772e4c944f28a124b735e
L <my917@qq.com>
2016-07-22 13:35:17 +0800

    update

--- nodejs/inflected/test1.js
+++ nodejs/inflected/test1.js
@@ -3,3 +3,5 @@ var Inflector = require('inflected');
 console.log(Inflector.pluralize('Category') ); // => 'Categories'
 console.log(Inflector.classify('poSt') );
 console.log(Inflector.camelize('fooBar') );
+console.log(Inflector.camelize('Test Timeout', false));
+console.log(Inflector.camelize('Test Timeout'));
