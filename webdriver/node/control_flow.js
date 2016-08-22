a997ccd62eee32ed25a0f78d6877f6d74b46c036
L <my917@qq.com>
2016-08-22 19:16:02 +0800

    update

---
+++ webdriver/node/control_flow.js
@@ -0,0 +1,22 @@
+var webdriver  = require('selenium-webdriver');
+var flow = webdriver.promise.controlFlow();
+
+var driver = new webdriver.Builder().
+withCapabilities(webdriver.Capabilities.chrome()).
+build();
+
+flow.execute(function () {
+    var d = webdriver.promise.defer();
+    driver.get('http://www.baidu.com').then(function (val) {
+        d.fulfill(val);
+    });
+    return d.promise;
+});
+flow.execute(function () {
+    console.log(1);
+    console.log(arguments);
+});
+flow.execute(function () {
+    console.log(2);
+    console.log(arguments);
+});
