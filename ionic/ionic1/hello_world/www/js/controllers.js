84c30738bc54801766e1e481cbc9cc337fe2aa02
L <my917@qq.com>
2016-08-29 15:23:31 +0800

    测试 cordova camera plugin ，待真机测试

--- ionic/ionic1/hello_world/www/js/controllers.js
+++ ionic/ionic1/hello_world/www/js/controllers.js
@@ -11,8 +11,21 @@ angular.module('starter.controllers', [])
 
   s.setTestPlugin = function (value) {
     s.testPlugin = value;
-    console.log(s.testPlugin);
-  }
+  };
+
+  s.testCamera = function () {
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI });
+
+    function onSuccess(imageURI) {
+      var image = document.getElementById('myImage');
+      image.src = imageURI;
+    }
+
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
+  };
 
 }])
 
