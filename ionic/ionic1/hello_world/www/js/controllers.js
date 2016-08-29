d4c96c54ecc0e7ef6bf9befab715cb493b7e09ba
L <my917@qq.com>
2016-08-29 18:55:08 +0800

    update

--- ionic/ionic1/hello_world/www/js/controllers.js
+++ ionic/ionic1/hello_world/www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('TestCordovaCtrl',["$scope","$rootScope","$location" ,function(s,$rootScope,$location) {
   console.log("TestCordovaCtrl");
-  
+
   s.location = {};
   s.location.absUrl = $location.absUrl();
   s.location.host = $location.host();
@@ -43,6 +43,33 @@ angular.module('starter.controllers', [])
     function onFail(message) {
       alert('Failed because: ' + message);
     }
+  };
+
+  s.getGeolocation = function () {
+// onSuccess Callback
+// This method accepts a Position object, which contains the
+// current GPS coordinates
+//
+    var onSuccess = function(position) {
+      alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+    };
+
+// onError Callback receives a PositionError object
+//
+    function onError(error) {
+      alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
   };
 
 }])
