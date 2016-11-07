32e9757f4c18b8ae5fd23b509fe95f6a9800bbee
L <my917@qq.com>
2016-11-07 11:01:24 +0800

    fs-extra  ensureDir  lesson

--- nodejs/file_system/fs_extra.js
+++ nodejs/file_system/fs_extra.js
@@ -1,17 +1,24 @@
 var fs = require('fs-extra');
 
-fs.remove(__dirname + '/test', function (err) {
-    if (err) return console.error(err);
-    fs.mkdir(__dirname + '/test', 0666, function (err) {
-        console.log('创建test目录');
-    });
-    if(err) {
-        console.log('删除空目录失败，可能原因：1、目录不存在，2、目录不为空')
-        console.error(err);
-        return;
-    }
-    console.log('success!')
+var dir = 'd:/tmp/this/path/does/not/exist';
+fs.ensureDir(dir, function(err) {
+    console.log(err); // => null
+    //dir has now been created, including the directory it is to be placed in
 });
+
+
+// fs.remove(__dirname + '/test', function (err) {
+//     if (err) return console.error(err);
+//     fs.mkdir(__dirname + '/test', 0666, function (err) {
+//         console.log('创建test目录');
+//     });
+//     if(err) {
+//         console.log('删除空目录失败，可能原因：1、目录不存在，2、目录不为空')
+//         console.error(err);
+//         return;
+//     }
+//     console.log('success!')
+// });
 
 
 // fs.rmdir(__dirname + '/test', function (err) {
