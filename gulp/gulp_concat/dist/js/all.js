bce1f79724c2d4676315a9f81fb34c559c1c14dd
L <my917@qq.com>
2016-11-14 15:33:16 +0800

    gulp concat lesson

---
+++ gulp/gulp_concat/dist/js/all.js
@@ -0,0 +1,3 @@
+console.log("test1");
+console.log("test2");
+console.log("test3");
---
+++ gulp/gulp_concat/gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat');
+
+gulp.task('testConcat', function () {
+    gulp.src('src/js/*.js')
+        .pipe(concat('all.js'))//合并后的文件名
+        .pipe(gulp.dest('dist/js'));
+});
