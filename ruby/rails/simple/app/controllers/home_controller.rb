5acdc246448d75d82dd03f7052f69e62985d8a8b
L <my917@qq.com>
2016-12-19 11:55:55 +0800

    rails simple add mongoid

--- ruby/rails/simple/app/controllers/home_controller.rb
+++ ruby/rails/simple/app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
     puts "home.index"
+    Article.new(name: "new article").save()
+    @articles = Article.all()
+    puts @articles
   end
 end
---
+++ ruby/rails/simple/app/models/article.rb
@@ -0,0 +1,4 @@
+class Article
+  include Mongoid::Document
+  field :name, type: String
+end
