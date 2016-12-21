3c4ae93369af48f988628401aa5cceda68174fe3
L <my917@qq.com>
2016-12-21 10:10:38 +0800

    rails simple 使用国际化 I18n

--- ruby/rails/simple/app/controllers/home_controller.rb
+++ ruby/rails/simple/app/controllers/home_controller.rb
@@ -4,5 +4,8 @@ class HomeController < ApplicationController
     Article.new(name: "new article").save()
     @articles = Article.all()
     puts @articles
+    puts I18n.t 'hello'
+    I18n.locale = :zh
+    puts I18n.t 'hello'
   end
 end
