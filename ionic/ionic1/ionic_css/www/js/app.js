4c0f04600a71f98b6ac8491015465df7550430ae
L <my917@qq.com>
2016-12-26 15:49:18 +0800

    update

--- ionic/ionic1/ionic_css/www/js/app.js
+++ ionic/ionic1/ionic_css/www/js/app.js
@@ -1,121 +1,117 @@
-// Ionic Starter App
+define(['ionic.bundle'], function (angular) {
+    var starter = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngCookies'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngCookies'])
+        .run(["$rootScope", "$ionicPlatform","$http", function ($rootScope, $ionicPlatform,$http) {
+            console.log($ionicPlatform);
+            $ionicPlatform.ready(function () {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
 
-    .run(["$rootScope", "$ionicPlatform","$http", function ($rootScope, $ionicPlatform,$http) {
-        console.log($ionicPlatform);
-        $ionicPlatform.ready(function () {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
+                if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                    cordova.plugins.Keyboard.disableScroll(true);
+                    console.log("cordova ready");
+                }
+                if (window.StatusBar) {
+                    // org.apache.cordova.statusbar required
+                    StatusBar.styleDefault();
+                }
+            });
 
-            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                cordova.plugins.Keyboard.disableScroll(true);
-                console.log("cordova ready");
-            }
-            if (window.StatusBar) {
-                // org.apache.cordova.statusbar required
-                StatusBar.styleDefault();
-            }
+            //启动微信js接口
+            $http({
+                url: '/api/weixin/config' ,
+                method: 'GET'
+            }).success(function (res) {
+                console.log(res);
+                wx.config(res.data);
+                wx.ready(function(){
+                    console.log(arguments);
+                });
+                wx.error(function(res){
+                    console.log(arguments);
+                });
+            });
+
+        }])
+
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('m1', {
+                    url: '/m1',
+                    views: {
+                        'index': {
+                            templateUrl: 'templates/m1.html',
+                            controller: 'M1Ctrl'
+                        }
+                    }
+                })
+                .state('m1.tabs', {
+                    url: '/tabs',
+                    views: {
+                        'm1': {
+                            templateUrl: 'templates/m1/tabs.html',
+                            controller: 'M1_TabsCtrl'
+                        }
+                    }
+                })
+                .state('m1.tabs.content', {
+                    url: '/content',
+                    views: {
+                        'tabs': {
+                            templateUrl: 'templates/m1/tabs/content.html',
+                            controller: 'M1_Tabs_ContentCtrl'
+                        }
+                    }
+                })
+                .state('m1.tabs.js', {
+                    url: '/js',
+                    views: {
+                        'tabs': {
+                            templateUrl: 'templates/m1/tabs/js.html',
+                            controller: 'M1_Tabs_JsCtrl'
+                        }
+                    }
+                })
+                .state('m1.tabs.2level', {
+                    url: '/2level',
+                    views: {
+                        'tabs': {
+                            templateUrl: 'templates/m1/tabs/2level.html',
+                            controller: 'M1_Tabs_2levelCtrl'
+                        }
+                    }
+                })
+                .state('m1.tabs.2level.1', {
+                    url: '/1',
+                    views: {
+                        '2level': {
+                            templateUrl: 'templates/m1/tabs/2level/1.html',
+                            controller: 'M1_Tabs_2level_1Ctrl'
+                        }
+                    }
+                })
+                .state('m1.tabs.2level.2', {
+                    url: '/2',
+                    views: {
+                        '2level': {
+                            templateUrl: 'templates/m1/tabs/2level/2.html',
+                            controller: 'M1_Tabs_2level_2Ctrl'
+                        }
+                    }
+                })
+                .state('m1.tabs.settings', {
+                    url: '/settings',
+                    views: {
+                        'tabs': {
+                            templateUrl: 'templates/m1/tabs/settings.html',
+                            controller: 'M1_Tabs_SettingsCtrl'
+                        }
+                    }
+                });
+
+            $urlRouterProvider.otherwise('/m1/tabs');
+
         });
-
-        //启动微信js接口
-        $http({
-            url: '/api/weixin/config' ,
-            method: 'GET'
-        }).success(function (res) {
-            console.log(res);
-            wx.config(res.data);
-            wx.ready(function(){
-                console.log(arguments);
-            });
-            wx.error(function(res){
-                console.log(arguments);
-            });
-        });
-
-    }])
-
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('m1', {
-                url: '/m1',
-                views: {
-                    'index': {
-                        templateUrl: 'templates/m1.html',
-                        controller: 'M1Ctrl'
-                    }
-                }
-            })
-            .state('m1.tabs', {
-                url: '/tabs',
-                views: {
-                    'm1': {
-                        templateUrl: 'templates/m1/tabs.html',
-                        controller: 'M1_TabsCtrl'
-                    }
-                }
-            })
-            .state('m1.tabs.content', {
-                url: '/content',
-                views: {
-                    'tabs': {
-                        templateUrl: 'templates/m1/tabs/content.html',
-                        controller: 'M1_Tabs_ContentCtrl'
-                    }
-                }
-            })
-            .state('m1.tabs.js', {
-                url: '/js',
-                views: {
-                    'tabs': {
-                        templateUrl: 'templates/m1/tabs/js.html',
-                        controller: 'M1_Tabs_JsCtrl'
-                    }
-                }
-            })
-            .state('m1.tabs.2level', {
-                url: '/2level',
-                views: {
-                    'tabs': {
-                        templateUrl: 'templates/m1/tabs/2level.html',
-                        controller: 'M1_Tabs_2levelCtrl'
-                    }
-                }
-            })
-            .state('m1.tabs.2level.1', {
-                url: '/1',
-                views: {
-                    '2level': {
-                        templateUrl: 'templates/m1/tabs/2level/1.html',
-                        controller: 'M1_Tabs_2level_1Ctrl'
-                    }
-                }
-            })
-            .state('m1.tabs.2level.2', {
-                url: '/2',
-                views: {
-                    '2level': {
-                        templateUrl: 'templates/m1/tabs/2level/2.html',
-                        controller: 'M1_Tabs_2level_2Ctrl'
-                    }
-                }
-            })
-            .state('m1.tabs.settings', {
-                url: '/settings',
-                views: {
-                    'tabs': {
-                        templateUrl: 'templates/m1/tabs/settings.html',
-                        controller: 'M1_Tabs_SettingsCtrl'
-                    }
-                }
-            });
-
-        $urlRouterProvider.otherwise('/m1/tabs');
-
-    });
+    return starter;
+});
--- ionic/ionic1/ionic_css/www/js/controllers.js
+++ ionic/ionic1/ionic_css/www/js/controllers.js
@@ -1,133 +1,136 @@
-angular.module('starter.controllers', [])
-    .controller('M1Ctrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
-        console.log("M1Ctrl");
-    }])
-    .controller('M1_TabsCtrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
-        console.log("M1_TabsCtrl");
-        s.setTabName = function (value) {
-            s.tabName = value;
-        };
-    }])
-    .controller('M1_Tabs_ContentCtrl', ["$scope","$scope" ,"$rootScope","$ionicModal", function (s,$scope,$rootScope, $ionicModal) {
-        console.log("M1_Tabs_ContentCtrl");
-        s.setTabName("content");
-        $ionicModal.fromTemplateUrl('templates/modal/modal.html', {
-            // scope: $scope,
-            scope: $scope,
-            animation: 'slide-in-up',
-            controller:function () {
-                console.log(2);
-            }
-        }).then(function(modal) {
-            console.log("init");
-            $scope.modal = modal;
-        });
-        $scope.openModal = function() {
-            console.log("openModal");
-            $scope.modal.show();
-        };
-        $scope.closeModal = function() {
-            console.log("closeModal");
-            $scope.modal.hide();
-        };
-        //当我们用到模型时，清除它！
-        $scope.$on('$destroy', function() {
-            console.log('$destroy');
-            $scope.modal.remove();
-        });
-        // 当隐藏的模型时执行动作
-        $scope.$on('modal.hide', function() {
-            console.log('modal.hide');
-            // 执行动作
-        });
-        // 当移动模型时执行动作
-        $scope.$on('modal.removed', function() {
-            console.log('modal.removed');
-            // 执行动作
-        });
+define(['ionic.bundle'], function (angular) {
+    var controllers = angular.module('starter.controllers', [])
+        .controller('M1Ctrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
+            console.log("M1Ctrl");
+        }])
+        .controller('M1_TabsCtrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
+            console.log("M1_TabsCtrl");
+            s.setTabName = function (value) {
+                s.tabName = value;
+            };
+        }])
+        .controller('M1_Tabs_ContentCtrl', ["$scope","$scope" ,"$rootScope","$ionicModal", function (s,$scope,$rootScope, $ionicModal) {
+            console.log("M1_Tabs_ContentCtrl");
+            s.setTabName("content");
+            $ionicModal.fromTemplateUrl('templates/modal/modal.html', {
+                // scope: $scope,
+                scope: $scope,
+                animation: 'slide-in-up',
+                controller:function () {
+                    console.log(2);
+                }
+            }).then(function(modal) {
+                console.log("init");
+                $scope.modal = modal;
+            });
+            $scope.openModal = function() {
+                console.log("openModal");
+                $scope.modal.show();
+            };
+            $scope.closeModal = function() {
+                console.log("closeModal");
+                $scope.modal.hide();
+            };
+            //当我们用到模型时，清除它！
+            $scope.$on('$destroy', function() {
+                console.log('$destroy');
+                $scope.modal.remove();
+            });
+            // 当隐藏的模型时执行动作
+            $scope.$on('modal.hide', function() {
+                console.log('modal.hide');
+                // 执行动作
+            });
+            // 当移动模型时执行动作
+            $scope.$on('modal.removed', function() {
+                console.log('modal.removed');
+                // 执行动作
+            });
 
-    }])
-    .controller('M1_Tabs_JsCtrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
-        console.log("M1_Tabs_JsCtrl");
-        s.setTabName("JS");
-        s.scanQRCode = function () {
-            wx.scanQRCode({
-                needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-                scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-                success: function (res) {
-                    console.log(1);
-                    var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-                    console.log(res);
-                    s.scanQRCodeRes = JSON.stringify(res);
-                    s.$apply();
-                }
-            });
-        };
-        s.getLocation = function () {
-            wx.getLocation({
-                type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-                success: function (res) {
-                    var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                    var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                    var speed = res.speed; // 速度，以米/每秒计
-                    var accuracy = res.accuracy; // 位置精度
-                    console.log(res);
-                    s.getLocationRes = JSON.stringify(res);
-                    s.$apply();
-                }
-            });
-        };
-        s.openLocation = function () {
-            wx.getLocation({
-                type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-                success: function (res) {
-                    var location = res;
-                    location.name = "name";
-                    location.address = "address";
-                    location.scale = "1";
-                    location.infoUrl = "http://www.baidu.com";
-                    wx.openLocation(location);
-                }
-            });
-        };
-        s.getNetworkType = function () {
-            wx.getNetworkType({
-                success: function (res) {
-                    var networkType = res.networkType; // 返回网络类型2g，3g，4g，wifi
-                    console.log(networkType);
-                    s.getNetworkTypeRes = networkType;
-                    s.$apply();
-                }
-            });
-        };
-    }])
-    .controller('M1_Tabs_2levelCtrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
-        console.log("M1_Tabs_2levelCtrl");
-        s.setTabName("Two Level");
-    }])
-    .controller('M1_Tabs_2level_1Ctrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
-        console.log("M1_Tabs_2level_1Ctrl");
-    }])
-    .controller('M1_Tabs_2level_2Ctrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
-        console.log("M1_Tabs_2level_2Ctrl");
-    }])
-    .controller('M1_Tabs_SettingsCtrl', ["$scope", "$rootScope", "$location","$cookies","$http", function (s, $rootScope, $location,$cookies,$http) {
-        console.log("M1_Tabs_SettingsCtrl");
-        s.setTabName("Settings");
-        s.getOpenID = function () {
-            console.log("getOpenID");
-            window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx13f06fd9ec831ed6&redirect_uri=http%3A%2F%2Fweixin.yangtuos.com%2FOAuth&response_type=code&scope=snsapi_base&state="+encodeURIComponent("/#/m1/tabs/settings")+"#wechat_redirect";
-        };
-        s.openid = $cookies.get("openid");
-        // console.log(encodeURI("#"));
-        // console.log(encodeURIComponent("#"));
+        }])
+        .controller('M1_Tabs_JsCtrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
+            console.log("M1_Tabs_JsCtrl");
+            s.setTabName("JS");
+            s.scanQRCode = function () {
+                wx.scanQRCode({
+                    needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                    scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                    success: function (res) {
+                        console.log(1);
+                        var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+                        console.log(res);
+                        s.scanQRCodeRes = JSON.stringify(res);
+                        s.$apply();
+                    }
+                });
+            };
+            s.getLocation = function () {
+                wx.getLocation({
+                    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+                    success: function (res) {
+                        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                        var speed = res.speed; // 速度，以米/每秒计
+                        var accuracy = res.accuracy; // 位置精度
+                        console.log(res);
+                        s.getLocationRes = JSON.stringify(res);
+                        s.$apply();
+                    }
+                });
+            };
+            s.openLocation = function () {
+                wx.getLocation({
+                    type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+                    success: function (res) {
+                        var location = res;
+                        location.name = "name";
+                        location.address = "address";
+                        location.scale = "1";
+                        location.infoUrl = "http://www.baidu.com";
+                        wx.openLocation(location);
+                    }
+                });
+            };
+            s.getNetworkType = function () {
+                wx.getNetworkType({
+                    success: function (res) {
+                        var networkType = res.networkType; // 返回网络类型2g，3g，4g，wifi
+                        console.log(networkType);
+                        s.getNetworkTypeRes = networkType;
+                        s.$apply();
+                    }
+                });
+            };
+        }])
+        .controller('M1_Tabs_2levelCtrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
+            console.log("M1_Tabs_2levelCtrl");
+            s.setTabName("Two Level");
+        }])
+        .controller('M1_Tabs_2level_1Ctrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
+            console.log("M1_Tabs_2level_1Ctrl");
+        }])
+        .controller('M1_Tabs_2level_2Ctrl', ["$scope", "$rootScope", "$location", function (s, $rootScope, $location) {
+            console.log("M1_Tabs_2level_2Ctrl");
+        }])
+        .controller('M1_Tabs_SettingsCtrl', ["$scope", "$rootScope", "$location","$cookies","$http", function (s, $rootScope, $location,$cookies,$http) {
+            console.log("M1_Tabs_SettingsCtrl");
+            s.setTabName("Settings");
+            s.getOpenID = function () {
+                console.log("getOpenID");
+                window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx13f06fd9ec831ed6&redirect_uri=http%3A%2F%2Fweixin.yangtuos.com%2FOAuth&response_type=code&scope=snsapi_base&state="+encodeURIComponent("/#/m1/tabs/settings")+"#wechat_redirect";
+            };
+            s.openid = $cookies.get("openid");
+            // console.log(encodeURI("#"));
+            // console.log(encodeURIComponent("#"));
 
-        s.updateAccessToken = function () {
-            $http({
-                url: '/api/access_tokens' ,
-                method: 'PUT'
-            }).success(function (res) {
-                console.log(res);
-            });
-        };
-    }]);
+            s.updateAccessToken = function () {
+                $http({
+                    url: '/api/access_tokens' ,
+                    method: 'PUT'
+                }).success(function (res) {
+                    console.log(res);
+                });
+            };
+        }]);
+    return controllers;
+});
