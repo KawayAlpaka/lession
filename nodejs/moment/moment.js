31b2588b04f819071ef5d4d454de82d2baa8dad4
L <my917@qq.com>
2017-01-03 16:57:03 +0800

    add moment lesson

---
+++ nodejs/moment/moment.js
@@ -0,0 +1,7 @@
+var moment = require('moment');
+
+console.log(moment().format());
+console.log(new Date());
+console.log(new Date().getTime());
+console.log(Date.now());
+console.log(moment(new Date("2015-09-15").getTime()).format());
