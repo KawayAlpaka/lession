35c4ccd2b2dc1dd6b95d1c57acefc73f020fef10
L <my917@qq.com>
2017-01-03 17:10:24 +0800

    update

--- nodejs/moment/moment.js
+++ nodejs/moment/moment.js
@@ -5,3 +5,4 @@ console.log(new Date());
 console.log(new Date().getTime());
 console.log(Date.now());
 console.log(moment(new Date("2015-09-15").getTime()).format());
+console.log(moment(new Date("2015-09-15 23:12:59:543").getTime()).format("YYYYMMDDHHmmssSSS"));
