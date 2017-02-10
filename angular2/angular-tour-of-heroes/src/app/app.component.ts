5e278e033c0fc4ecc894afc0b4732081c876b975
L <my917@qq.com>
2017-02-10 14:12:09 +0800

    hero lesson 5 服务

--- angular2/angular-tour-of-heroes/src/app/app.component.ts
+++ angular2/angular-tour-of-heroes/src/app/app.component.ts
@@ -1,22 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Hero } from './hero';
-
-
-const HEROES:Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
+  providers: [HeroService],
   template: `
   <h1>{{title}}</h1>
   <my-hero-detail [hero]="selectedHero"></my-hero-detail>
@@ -82,8 +70,18 @@ const HEROES:Hero[] = [
 export class AppComponent {
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+
 }
