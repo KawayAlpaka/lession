4cb9fb1e341dfbb4662ce3e54c815902c04bf66a
L <my917@qq.com>
2017-02-10 16:34:33 +0800

    hero lesson 6.3  重构 routing 模块

--- angular2/angular-tour-of-heroes/src/app/app.module.ts
+++ angular2/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,28 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import {RouterModule }   from '@angular/router';
-
-
+import { AppRoutingModule } from './app-routing.module'
 
 import { HeroService } from './hero.service';
 
 import { AppComponent }  from './app.component';
-
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
-import { DashboardComponent } from './dashboard.component'
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {path: 'heroes', component: HeroesComponent},
-      {path: 'dashboard', component: DashboardComponent},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-      {path: 'detail/:id', component: HeroDetailComponent}
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
