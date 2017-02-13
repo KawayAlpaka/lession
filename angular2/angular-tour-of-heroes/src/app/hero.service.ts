f5858bbdd95332a21426e9fc27445fd0fd84bfa6
L <my917@qq.com>
2017-02-13 13:55:42 +0800

    hero lesson 7.2 英雄增删改

--- angular2/angular-tour-of-heroes/src/app/hero.service.ts
+++ angular2/angular-tour-of-heroes/src/app/hero.service.ts
@@ -35,4 +35,32 @@ export class HeroService {
     return this.getHeroes()
       .then(heroes => heroes.find(hero => hero.id === id));
   }
+
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
+  
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
 }
