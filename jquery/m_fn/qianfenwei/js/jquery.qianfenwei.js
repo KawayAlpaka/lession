4c0786a6999ae742e400c6ed083402fb26aa1134
L <my917@qq.com>
2017-04-07 15:57:34 +0800

    jquery.qianfenwei

---
+++ jquery/m_fn/qianfenwei/js/jquery.qianfenwei.js
@@ -0,0 +1,43 @@
+(function ($) {
+    var set_text_value_position = function(input, spos){
+        if(spos<0)
+            spos = 0;
+        if(input.setSelectionRange){ //兼容火狐,谷歌
+            setTimeout(function(){
+                    input.setSelectionRange(spos, spos);
+                    input.focus();}
+                ,0);
+        }else if(input.createTextRange){ //兼容IE
+            var rng = input.createTextRange();
+            rng.move('character', spos);
+            rng.select();
+        }
+    };
+
+    $.fn.qianfenwei = function () {
+        var oldValue = this.value || "";
+        var newValue = this.value || "";
+        var qianfenwei = function () {
+            var self = this;
+            var _tempPos = self.selectionStart;
+            var oldCommaCount = oldValue.match(/，/g) ?  oldValue.match(/，/g).length : 0;
+            var _temp = self.value.replace(/[^0-9\.]/g,"");
+
+            var arrText = _temp.split(".");
+            var int = arrText[0];
+            self.value = int.replace(/，/g, '').replace(/\d+?(?=(?:\d{3})+$)/g, function (s) {
+                return s + '，';
+            });
+            if(arrText.length >1){
+                self.value += "."+arrText[1].substr(0,2);
+            }
+            newValue = self.value;
+            var newCommaCount = newValue.match(/，/g) ?  newValue.match(/，/g).length : 0;
+            var commaChazhi = newCommaCount - oldCommaCount;
+            set_text_value_position(self,_tempPos+commaChazhi);
+            oldValue = newValue;
+        };
+        $(this).unbind("input");
+        $(this).bind("input",qianfenwei);
+    }
+}($));
