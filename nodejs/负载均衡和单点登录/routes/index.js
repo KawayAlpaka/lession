af86ab646fb3b246476d900181c3b2194489b762
L <my917@qq.com>
2017-04-08 13:04:09 +0800

    负载均衡 完成 ，还不能宕机快速切换

--- nodejs/负载均衡和单点登录/routes/index.js
+++ nodejs/负载均衡和单点登录/routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [];
+
+var session = require('express-session');
+var RedisStrore = require('connect-redis')(session);
+
+router.use(session({
+  secret: 'secret',
+  cookie: {
+    maxAge: 1000 * 60 * 30
+  },
+  store : new RedisStrore({
+    "host" : "localhost",
+    "port" : "6379",
+    "db" : 1
+  })
+}));
+
+router.use(function(req,res,next){
+  res.locals.user = req.session.user;   // 从session 获取 user对象
+  var err = req.session.error;   //获取错误信息
+  delete req.session.error;
+  res.locals.message = "";   // 展示的信息 message
+  if(err){
+    res.locals.message = '<div class="alert alert-danger" style="margin-bottom:20px;color:red;">'+err+'</div>';
+  }
+  next();  //中间件传递
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',locals:res.locals,env:process.env});
+});
+router.post('/signin',function (req, res, next) {
+  if(req.body.user){
+    req.session.user = req.body.user;
+    users.push(req.session.user);
+  }
+  res.redirect("/");
+});
+router.get('/signout',function (req, res, next) {
+  req.session.user = undefined;
+
+  res.redirect("/");
 });
 
 module.exports = router;
