3923542738e7f664a81514d92ce34144c81e4a17
L <my917@qq.com>
2017-04-14 09:01:10 +0800

    update 千分位

--- jquery/m_fn/qianfenwei/js/jquery.qianfenwei.js
+++ jquery/m_fn/qianfenwei/js/jquery.qianfenwei.js
@@ -15,29 +15,35 @@
     };
 
     $.fn.qianfenwei = function () {
-        var oldValue = this.value || "";
-        var newValue = this.value || "";
-        var qianfenwei = function () {
-            var self = this;
-            var _tempPos = self.selectionStart;
-            var oldCommaCount = oldValue.match(/，/g) ?  oldValue.match(/，/g).length : 0;
-            var _temp = self.value.replace(/[^0-9\.]/g,"");
+        this.each(function (i,ele) {
+            var oldValue = ele.value || "";
+            var newValue = ele.value || "";
+            var qianfenwei = function () {
+                var self = this;
+                var _tempPos = self.selectionStart;
+                var oldCommaCount = oldValue.match(/，/g) ?  oldValue.match(/，/g).length : 0;
+                var _temp = self.value.replace(/[^0-9\.]/g,"");
 
-            var arrText = _temp.split(".");
-            var int = arrText[0];
-            self.value = int.replace(/，/g, '').replace(/\d+?(?=(?:\d{3})+$)/g, function (s) {
-                return s + '，';
-            });
-            if(arrText.length >1){
-                self.value += "."+arrText[1].substr(0,2);
-            }
-            newValue = self.value;
-            var newCommaCount = newValue.match(/，/g) ?  newValue.match(/，/g).length : 0;
-            var commaChazhi = newCommaCount - oldCommaCount;
-            set_text_value_position(self,_tempPos+commaChazhi);
-            oldValue = newValue;
-        };
-        $(this).unbind("input");
-        $(this).bind("input",qianfenwei);
+                var arrText = _temp.split(".");
+                var int = arrText[0];
+                self.value = int.replace(/，/g, '').replace(/\d+?(?=(?:\d{3})+$)/g, function (s) {
+                    return s + '，';
+                });
+                if(arrText.length >1){
+                    self.value += "."+arrText[1].substr(0,2);
+                }
+                newValue = self.value;
+                var newCommaCount = newValue.match(/，/g) ?  newValue.match(/，/g).length : 0;
+                var commaChazhi = newCommaCount - oldCommaCount;
+                if(Math.abs(commaChazhi) > 0 ){
+                    console.log(commaChazhi);
+                }
+                set_text_value_position(self,_tempPos+commaChazhi);
+                oldValue = newValue;
+            };
+            $(ele).unbind("input");
+            $(ele).bind("input",qianfenwei);
+        });
+
     }
 }($));
