6a32c090d584c029137377fc4e5ee40bb7d869f9
L <my917@qq.com>
2017-04-27 09:33:10 +0800

    update

--- js/ES6/generator/coTest.js
+++ js/ES6/generator/coTest.js
@@ -2,15 +2,28 @@ var co = require('co');
 var getInfo = function (data) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
-            resolve(data + 1);
+            var random = Math.random();
+            if(random<0.5){
+                resolve(data + 1);
+            }else {
+                reject(data +":"+random);
+            }
+
         },1000)
     });
 };
 co(function* () {
-    var r1 = yield getInfo(0);
-    console.log(r1);
-    var r2 = yield getInfo(r1);
-    console.log(r2);
+    while (true){
+        try {
+            var r1 = yield getInfo(0);
+            console.log(r1);
+            var r2 = yield getInfo(r1);
+            console.log(r2);
+        }catch (e){
+            console.log(e);
+        }
+
+    }
     return getInfo(r2);
 }).then(function (data) {
     console.log(data);
