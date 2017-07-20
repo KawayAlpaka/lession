7f6883b43ad0d0f2527b0b5349257ae59ecc251c
L <my917@qq.com>
2017-07-20 09:56:41 +0800

    update

--- socket.io/www/scripts/hichat.js
+++ socket.io/www/scripts/hichat.js
@@ -6,9 +6,18 @@ var socket=io.connect(),//与服务器进行连接
     messageBoxWho=document.getElementById('message-box-who'),
     messageBoxText=document.getElementById('message-box-text'),
     text=document.getElementById('text');
-button.onclick=function(){
+form.addEventListener("submit",function () {
     socket.emit('foo',{text:text.value,who:who.value});//发送一个名为foo的事件，并且传递一个字符串数据‘hello’
+    text:text.value = "";
+    return false;
+});
+form.onsubmit = function () {
+    return false;
 };
+// button.onclick=function(){
+//     socket.emit('foo',{text:text.value,who:who.value});//发送一个名为foo的事件，并且传递一个字符串数据‘hello’
+//     text:text.value = "";
+// };
 function showMessageBox(who,text) {
     messageBoxWho.innerText = who;
     messageBoxText.innerText = text;
@@ -23,9 +32,12 @@ socket.on("foo2",function (data) {
     console.log(data);
     var div = document.createElement("div");
     div.className = "message";
-    div.innerHTML = "<div class='message'><div class='who'>"+data.who+"</div><div class='text'>"+data.text+"</div></div>";
+    div.innerHTML = "<div class='message'><div class='who'>"+data.who+":</div><div class='text'>"+data.text+"</div></div>";
+    if(data.who != who.value){
+        new Notification(data.who, {body: data.text});
+    }
     panel.appendChild(div);
-    showMessageBox(data.who,data.text);
+    // showMessageBox(data.who,data.text);
 });
 socket.on("userCount",function (count) {
     userCount.innerText = count;
