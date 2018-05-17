14861c25ce34ed9bd78ed8d83fea11cbfb80d5a4
L <my917@qq.com>
2018-05-17 17:28:40 +0800

    my bind

--- js/call_apply_bind/bind.js
+++ js/call_apply_bind/bind.js
@@ -1,3 +1,23 @@
+//自定义bind方法
+// let log = console.log;//必须有一个这个，里面才能console.log 有点奇怪
+let myBind = function(){
+    let bind = function(o,...args1){
+        // console.log("my bind");
+        var self = this;
+        return function(...args2){
+            var args;
+            if(args1){
+                args = args1.concat(args2);
+            }else{
+                args = args2;
+            }
+            self.apply(o,args);
+        };
+    };
+    Function.prototype.bind = bind;
+};
+// myBind();
+
 var objA = {};
 objA.name = "A";
 objA.funcA = function (str1,str2) {
