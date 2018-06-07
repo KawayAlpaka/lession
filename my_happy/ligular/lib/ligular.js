6cc852d4ecb25cd983e2b08213c4d280bed02c50
L <my917@qq.com>
2018-06-07 15:21:27 +0800

    update

--- my_happy/ligular/lib/ligular.js
+++ my_happy/ligular/lib/ligular.js
@@ -1,5 +1,6 @@
-const ARROW_ARG = /^([^(]+?)=>/;
-const FN_ARGS = /^[^(]*\(\s*([^)]*)\)/m;
+//从angularjs源码上取的正则
+const ARROW_ARG = /^([^(]+?)=>/;            //处理没有括号的箭头函数定义方式
+const FN_ARGS = /^[^(]*\(\s*([^)]*)\)/m;    //处理有括号的函数定义方式
 
 class Injector{
     constructor(){
@@ -7,8 +8,11 @@ class Injector{
     }
     getArgs(fn){
         let fnText = fn.toString();
+        console.log(fnText);
         let args = fnText.match(ARROW_ARG) || fnText.match(FN_ARGS);
-        return args[1].split(",");
+        return args[1].split(",").map((arg)=>{
+            return arg.trim();
+        });
     }
 }
 //测试获取参数
@@ -16,7 +20,13 @@ class Injector{
     var testCases = [
         {fn:function(ab,cd){},except:"ab,cd"},
         {fn:(ab,cd)=>{},except:"ab,cd"},
+        {fn:(ab,cd)=>
+            {},except:"ab,cd"},
+        {fn:(ab , cd)=>{},except:"ab,cd"},
         {fn:ab=>{},except:"ab"},
+        {fn:ab=>
+            {},except:"ab"},
+        {fn:ab =>{},except:"ab"},
     ];
     let injector = new Injector();
     testCases.forEach((testCase)=>{
