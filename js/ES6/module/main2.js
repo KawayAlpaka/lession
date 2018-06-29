2943a5a1f1053f765657c9c37701bf9aeab566a9
L <my917@qq.com>
2018-06-29 15:51:32 +0800

    update

--- js/ES6/module/main2.js
+++ js/ES6/module/main2.js
@@ -1,5 +1,13 @@
 import {Hello,name1,name2} from "./common.mjs";
-
+import("/module-b.mjs")
+  .then(module => {
+    console.log(`module:`);
+    console.log(module);
+  })
+  .catch(err => {
+    console.log(`err:`);
+    console.log(err);
+  });
 
 Hello("Jim");
 Hello(name1);
