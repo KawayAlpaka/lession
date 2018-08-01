68719a27b76afdd0b4d8e5b7753cbc3b0e6e92e3
L <my917@qq.com>
2018-08-01 15:13:53 +0800

    update mRequire

--- nodejs/require/m_require.js
+++ nodejs/require/m_require.js
@@ -4,7 +4,11 @@ var r = function(path){
   var code = fs.readFileSync(path+".js",{encoding:"utf-8"});
   var module = {};
   (function(){
-    eval(code);
+    // eval 动态执行方案
+    // eval(code);
+    // Function 动态执行方案
+    var f = new Function("module",code);
+    f(module);
   })();
   return module.exports;
 };
