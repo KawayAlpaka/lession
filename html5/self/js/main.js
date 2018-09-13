b5dbbbd69ecf63c30f491b09b992ba9216a6a3ee
L <my917@qq.com>
2018-09-13 16:34:58 +0800

    html5 self

---
+++ html5/self/js/main.js
@@ -0,0 +1,7 @@
+// console.log(document.currentScript);
+console.log(location.pathname);
+console.log(`window === self:${window === self }`);
+console.log(`window.window === window.self:${window.window === window.self }`);
+console.log(`window.self === self:${window.self === self }`);
+console.log(`window.window === self:${window.window === self }`);
+console.log(`parent === self:${parent === self }`);
