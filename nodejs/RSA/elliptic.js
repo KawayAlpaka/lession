cb41f921a05d27ad8c5ba7ab1c882a81b7aacea6
L <my917@qq.com>
2018-12-11 17:40:39 +0800

    测试 elliptic

---
+++ nodejs/RSA/elliptic.js
@@ -0,0 +1,22 @@
+let EC = require("elliptic").ec;
+let ec = new EC('secp256k1');
+
+// 生成公私钥对
+let keypair = ec.genKeyPair();
+const res = {
+  prv: keypair.getPrivate('hex').toString(),
+  pub: keypair.getPublic('hex').toString(),
+}
+console.log(res);
+
+// 私钥签名,公钥验证前面
+let keypair2 = ec.genKeyPair();
+let data = "hahahehe";
+let prvKey = ec.keyFromPrivate(res.prv,'hex');
+let pubKey = ec.keyFromPublic(res.pub,'hex');
+// 另外的公钥测试
+// let pubKey = ec.keyFromPublic(keypair2.getPublic('hex').toString(),'hex');
+
+let signature = prvKey.sign(data);
+let derSign = signature.toDER();
+console.log(pubKey.verify(data, derSign));
