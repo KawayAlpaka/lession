8817de36ffedaa756188604c7f9ba70425e7cea4
L <my917@qq.com>
2019-06-11 11:33:42 +0800

    update  crawlers，添加标签过滤

--- nodejs/crawlers/gulongbbs.js
+++ nodejs/crawlers/gulongbbs.js
@@ -6,8 +6,8 @@ var fs = require("fs");
 var path = require("path");
 
 var baseUrl = "https://www.gulongbbs.com";
-var listUrl = "https://www.gulongbbs.com/book/jyyf/";
-var outputFile = "九月鹰飞.txt";
+var listUrl = "https://www.gulongbbs.com/book/tymyd/";
+var outputFile = "天涯.明月.刀.txt";
 var outputDir = "dist"
 var outputPath = path.resolve(__dirname,outputDir,outputFile);
 
@@ -30,10 +30,19 @@ axios.get(listUrl, {})
         let $ = cheerio.load(r.data);
         let title = $(".main_ArticleTitle").text();
         let content = $("#new>span").html();
-        // utf-8 字符串转中文字符串
+        
+        // 标签转换
         content = content.replace(/<br>/gi, "\r\n");
-        content = content.replace(/<p><\/p>/gi, "\r\n");
+        content = content.replace(/<p[\s\S]*?>([\s\S]*?)<\/p>/gi, "$1");
+        content = content.replace(/<strong[\s\S]*?>([\s\S]*?)<\/strong>/gi, "$1");
+        content = content.replace(/<span[\s\S]*?>([\s\S]*?)<\/span>/gi, "$1");
+        content = content.replace(/<font[\s\S]*?>([\s\S]*?)<\/font>/gi, "$1");
+        
+        // utf-8 字符串转中文字符串
         content = unescape(content.replace(/&#x/g, '%u').replace(/;/g, ''));
+
+        // 去除一些没有正常转换的符号
+        content = content.replace(/%uD7/g, "");
 
         if (!fs.existsSync("dist")) {
           fs.mkdirSync("dist");
