a5cc712281edd63cae9f4f6d87ae34abdbb5100e
L <my917@qq.com>
2019-09-03 08:49:42 +0800

    update

--- python/statistics/main_distribution.py
+++ python/statistics/main_distribution.py
@@ -1,36 +1,25 @@
 import numpy as np
-import random
-import matplotlib.mlab as mlab
+import random,math
 import matplotlib.pyplot as plt
-import math
-# from scipy import stats
 
-
-def demo1():
-  mu ,sigma = 0, 1
-  sampleNo = 1000
-  np.random.seed(0)
-  s = np.random.normal(mu, sigma, sampleNo)
-
-  plt.hist(s, bins=100, normed=True)
-  plt.show()
-
-def fenbu(data,rwidth=None):
+# 返回数据(频段数组,频数数组,频率数组)
+def get_rate(data,rwidth=1):
   len_data = len(data)
   min_data = math.floor(min(data))
   max_data = math.ceil(max(data)) 
-  if rwidth == None :
-    rwidth = (max_data - min_data) / 10
-  if rwidth < 1:
-    rwidth = 1
-  rwidth = math.floor(rwidth)
-  x = []
-  y = []
-  z = []
-  for i in range(min_data ,max_data,rwidth):
+
+  x = []  #频段
+  y = []  #频数
+  z = []  #频率
+
+  i = min_data
+  while i <= max_data + rwidth:
     x.append((i,i+rwidth,i+rwidth/2))
     y.append(0)
     z.append(0)
+    i += rwidth
+  i = 0
+
   for num in data:
     for index,r in enumerate(x):
       if num >= r[0] and num < r[1]:
@@ -38,18 +27,21 @@ def fenbu(data,rwidth=None):
   for index,num in enumerate(y):
     z[index] = y[index] / len_data
   return x,y,z
+
+#从数据中随机采样n个点
 def sample(data,n):
   return [data[random.randint(0,len(data)-1)] for _ in range(n)]
-  
+
+# 绘制数据的卡方分布图
 def chi_square(data):
-  mo = [(1,"red"),(2,"green"),(3,"brown"),(5,"brown"),(10,"darksalmon"),(20,"gold")]
+  mo = [(1,"red"),(3,"green"),(6,"brown"),(12,"gold"),(24,"darksalmon")]
   for n,color in mo:
     g1 = []
     for _ in range(1,10000):
       s = sample(data,n)
       x = sum([ num**2 for num in s ])
       g1.append(x)
-    f = fenbu(g1)
+    f = get_rate(g1,0.5)
     xx = [f0[2] for f0 in f[0]]
     yy = [f2 for f2 in f[2]]
     plt.plot(xx,yy,color)
@@ -57,6 +49,19 @@ def chi_square(data):
   plt.show()
 
 def t_distribution(data):
+  # mo = [(1,"red"),(3,"green"),(6,"brown"),(10,"darksalmon"),(20,"gold")]
+  # for n,color in mo:
+  #   g1 = []
+  #   for _ in range(1,10000):
+  #     s = sample(data,n)
+  #     x = sum([ num**2 for num in s ])
+  #     y = (sum([ num for num in s ]) /n) / math.sqrt(x/n)
+  #     g1.append(y)
+  #   f = get_rate(g1)
+  #   xx = [f0[2] for f0 in f[0]]
+  #   yy = [f2 for f2 in f[2]]
+  #   plt.plot(xx,yy,color)
+  # plt.show()
   pass
 
 if __name__ == "__main__":
