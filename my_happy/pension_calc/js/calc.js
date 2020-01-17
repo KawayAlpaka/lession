36f49f6d6246fb574125a728dda078f3d9b6804d
L <my917@qq.com>
2020-01-17 11:42:03 +0800

    养老金计算器

---
+++ my_happy/pension_calc/js/calc.js
@@ -0,0 +1,83 @@
+(function(){
+  var sum = function(...args){
+    var sum = 0;
+    args.forEach((arg)=>{
+      sum += arg;
+    });
+    return sum;
+  }
+
+  var socialFactorsEle = document.querySelector("#social-factors");
+  var personalFactorsEle = document.querySelector("#pasonal-factors");
+  var getFactors = function(){
+    var socialFactors = {};
+    var inputs = socialFactorsEle.querySelectorAll("input[name]");
+    inputs.forEach((input)=>{
+      socialFactors[input.name] = parseFloat(input.value);
+    });
+    var personalFactors = {};
+    inputs = personalFactorsEle.querySelectorAll("input[name]");
+    inputs.forEach((input)=>{
+      personalFactors[input.name] = parseFloat(input.value);
+    });
+    return {
+      socialFactors,
+      personalFactors
+    };
+  };
+
+  var showResult = function(result){
+    var resultEle = document.getElementById("result-panel");
+    Object.keys(result).forEach((key)=>{
+      let input = resultEle.querySelector(`input[name=${key}]`);
+      if(input){
+        input.value = result[key].toFixed(2);
+      }
+    });
+  };
+
+  var calc = function(){
+    var factors = getFactors();
+    console.log(factors);
+    var perZhishu = [];
+    var nianshu = 0;
+    var personalPaymentProportion = factors.socialFactors.personalPaymentProportion / 100;
+    var companyPaymentProportion = factors.socialFactors.companyPaymentProportion / 100;
+    var averageSalary = factors.socialFactors.averageSalary;
+    var averageSalaryIncreaseRatio = factors.socialFactors.averageSalaryIncreaseRatio / 100;
+    var payBase = factors.personalFactors.payBase;
+    var payBaseIncreaseRatio = factors.personalFactors.payBaseIncreaseRatio / 100;
+    var personalAccountAmount = 0;
+    var companyAccountAmount = 0;
+    for(let age = factors.personalFactors.startPayAge;age<factors.socialFactors.retirementAge;age++){
+      if(age < factors.personalFactors.endPayAge){
+        perZhishu.push(payBase / averageSalary);
+        nianshu++;
+        personalAccountAmount += payBase * personalPaymentProportion * 12;
+        companyAccountAmount += payBase * companyPaymentProportion * 12;
+      }
+      averageSalary = averageSalary * ( 1 + averageSalaryIncreaseRatio);
+      payBase = payBase * ( 1 + payBaseIncreaseRatio);
+    }
+
+    // 如果不是新开一年的情况
+    averageSalary = averageSalary / ( 1 + averageSalaryIncreaseRatio );
+
+    var result = {};
+    result.nianshu = nianshu;
+    result.zhishu = sum(...perZhishu) / nianshu;
+    result.averageSalary = averageSalary;
+    result.pensionBase = result.averageSalary * ( 1 + result.zhishu) / 2 * nianshu * 0.01;
+    result.personalAccountAmount = personalAccountAmount;
+    result.companyAccountAmount = companyAccountAmount;
+    result.pensionPersonal = personalAccountAmount / factors.socialFactors.monthlyCount;
+    result.pensionTotal = result.pensionBase + result.pensionPersonal;
+    console.log(result);
+    showResult(result);
+  };
+  calc();
+  document.getElementById("form").onsubmit = function(){
+    calc();
+    return false;
+  };
+})();
---
+++ my_happy/pension_calc/readme.md
@@ -0,0 +1,7 @@
+参考资料：
+
+https://www.zhihu.com/question/21811960
+
+https://baike.baidu.com/item/个人指数化工资     
+
+
