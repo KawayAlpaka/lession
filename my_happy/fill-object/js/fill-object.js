04882d25398dcd82bb7ab36c9db6ab332eef5610
L <my917@qq.com>
2020-09-21 15:21:27 +0800

    开发 fill-object 方法。作用是用 模板填充另一个对象。 引入 mocha 启动浏览器端测试

---
+++ my_happy/fill-object/js/fill-object.js
@@ -0,0 +1,25 @@
+const fillObject = (source, pattern) => {
+	var bianli = (source, pattern) => {
+		if (!pattern) {
+			return;
+		}
+		let keys = Object.keys(pattern);
+		for (let key of keys) {
+			// console.log(key);
+			let type = typeof source[key];
+			if (type == "undefined" || source[key] == null) {
+
+        if(typeof pattern[key] != "undefined"){
+          source[key] = pattern[key];
+        }
+			} else if (type == "object") {
+				if (typeof pattern[key] == "object") {
+					bianli(source[key], pattern[key]);
+				}
+			}
+		}
+	}
+	bianli(source, pattern);
+	return source;
+}
+
