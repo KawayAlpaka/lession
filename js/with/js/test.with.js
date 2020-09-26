30297338c51fa043a1936f73ee01be4193e23e62
L <my917@qq.com>
2020-09-26 16:54:19 +0800

    update

--- js/with/js/test.with.js
+++ js/with/js/test.with.js
@@ -10,8 +10,10 @@ with(a){
 
 // 要阻断b，就必须在调用时传入一个b的值
 var b = {b:"bb"}
+var g = {};
 var fn =  new Function("g", `
                 with(g){
+                  cc = "cc";
                   var hehe = "hehe";
                   console.log(this);
                   console.log(a);
@@ -20,5 +22,6 @@ var fn =  new Function("g", `
                   // alert("alert"); // 还是会弹出
                   return a;
                 }
-              `).bind({});
+              `).bind(g);
 fn({a,window:{},b:undefined});
+console.log(g);
