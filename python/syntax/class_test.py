436f507981e40212eb1a4c77421168956947167a
L <my917@qq.com>
2021-03-04 17:37:54 +0800

    update

---
+++ python/syntax/class_test.py
@@ -0,0 +1,10 @@
+class Foo:
+  pass
+class Bar:
+  pass
+
+foo = Foo()
+
+print(isinstance(foo,Foo))
+print(isinstance(foo,Bar))
+
---
+++ python/syntax/reg.py
@@ -0,0 +1,20 @@
+import re
+
+
+print(re.match('www', 'www.runoob.com').span())  # 在起始位置匹配
+print(re.match('www', 'www.runoob.com'))  # 不在起始位置匹配
+
+
+
+print(re.search('www', 'www.runoob.com').span())  # 在起始位置匹配
+print(re.search('com', 'www.runoob.com').span())         # 不在起始位置匹配
+print(re.search('comdd', 'www.runoob.com'))
+
+
+print(re.search('https://dg.anjuke.com/community/.*/', 'https://dg.anjuke.com/community/changan/'))
+print(re.search('https://dg.anjuke.com/community/.*/', 'https://dg.anjuke.com/community/changan/'))
+print(re.search('(^https:\/\/dg\.anjuke\.com\/community\/.*\/$)|(^https:\/\/dg\.anjuke\.com\/community\/.*\/p\d{1,3}\/$)', 'https://dg.anjuke.com/community/changan/p3/'))
+print(re.search('(^https:\/\/dg\.anjuke\.com\/community\/.*\/$)|(^https:\/\/dg\.anjuke\.com\/community\/.*\/p\d{1,3}\/$)', 'https://dg.anjuke.com/community/p1/'))
+
+
+
