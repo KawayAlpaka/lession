4cade5eb43418b22946beec3cd28760758b11168
L <my917@qq.com>
2021-03-12 13:50:07 +0800

    django 获取数据返回到页面

--- python/django/hello_world/blog/urls.py
+++ python/django/hello_world/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('hehe', views.HelloWorld, name='haha'),
+    path('articles', views.Articles, name='haha'),
 ]
--- python/django/hello_world/blog/views.py
+++ python/django/hello_world/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 # Create your views here.
 
 def HelloWorld(request):
   return HttpResponse("Hello, world")
+
+def Articles(request):
+  return HttpResponse(",".join([a.title for a in Article.objects.all()]))
