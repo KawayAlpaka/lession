01725dbb1b05f5a3b8c9b19543edb7f08a564214
L <my917@qq.com>
2021-03-19 14:21:43 +0800

    使用 python  打开 chrome ，带参数

--- python/syntax/web.py
+++ python/syntax/web.py
@@ -1,11 +1,24 @@
 # import webbrowser as web
 # web.open("http://www.baidu.com",new=0,autoraise=True)
-
+from webbrowser import Chrome
 import webbrowser
-chromePath = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chromePath))
-chrome = webbrowser.get('chrome')
-chrome.open('https://www.baidu.com',new=1,autoraise=True)
+
+
+# # chromePath = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+# # chromePath = r'C:\Users\dell\AppData\Local\Google\Chrome\Application\chrome.exe --args --disable-web-security --user-data-dir=D:\chrome'
+# chromePath = r'C:\Users\dell\AppData\Local\Google\Chrome\Application\chrome.exe'
+# # chromePath = r'C:\Users\dell\Desktop\Google Chrome-不跨域.lnk'
+# webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chromePath))
+# chrome = webbrowser.get(chromePath)
+# chrome.open('https://www.baidu.com',new=1,autoraise=True)
 
 
 
+
+new_chrome = webbrowser.Chrome()
+new_chrome.name = r'C:\Users\dell\AppData\Local\Google\Chrome\Application\chrome.exe'
+new_chrome.remote_args = webbrowser.Chrome.remote_args + [
+  "--disable-web-security",
+  "--user-data-dir=D:\chrome"
+]
+new_chrome.open("https://www.baidu.com")
