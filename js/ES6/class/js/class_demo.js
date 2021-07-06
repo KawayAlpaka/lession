6eaa70c96b55bf547c61ed3ec93fb872df1c60a9
L <my917@qq.com>
2021-07-06 10:38:11 +0800

    js class 继承并重写 Array demo

--- js/ES6/class/js/class_demo.js
+++ js/ES6/class/js/class_demo.js
@@ -34,3 +34,24 @@ Animal.callData();
 let dog = new Dog();
 dog.howl("haha");
 Dog.callData();
+
+
+
+class MyArray extends Array{
+  constructor(){
+    super()
+    // var methods = ["push","pop"];
+    // methods.forEach(name=>{
+    //   this[name] = (...args)=>{
+    //     super[name](...args);
+    //     console.log("change");
+    //   }
+    // });
+  }
+  push(...args){
+    super.push(...args);
+    console.log("change");
+  }
+}
+var myArray = new MyArray();
+console.log(myArray);
