8d720b83edd1f39a2401827da1b2eaecc35c91a6
L <my917@qq.com>
2021-07-09 09:20:25 +0800

    懒加载模块

---
+++ webpack/webpack5/src/common/lazy.js
@@ -0,0 +1,3 @@
+export const sub = (a,b)=>{
+  return a - b;
+}
--- webpack/webpack5/src/main.js
+++ webpack/webpack5/src/main.js
@@ -8,9 +8,22 @@ class Test{
 }
 
 console.log(Test._data);
-console.log(list,add(1,3));
+console.log("main",add(1,3));
 if(IsHappy){
 	console.log("happy")
 }else{
 	console.log("unhappy")
 }
+
+const button = document.createElement("button");
+button.innerHTML = "test"
+button.addEventListener("click",()=>{
+	import("./common/lazy").then((lazy)=>{
+		console.log(lazy);
+		console.log(lazy.sub(10,5)); 
+	});
+	// 下面这种方式就直接打包了，不会懒加载
+	// const lazy = require("./common/lazy")
+	// console.log(lazy);
+},false);
+document.body.appendChild(button);
