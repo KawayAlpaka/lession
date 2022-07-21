5408e2da3507387298b46931e6e003b6395ef3f3
L <my917@qq.com>
2022-07-21 18:44:14 +0800

    rollup打包ts，生成描述文件

--- rollup/multiple/script/build.ts
+++ rollup/multiple/script/build.ts
@@ -1,6 +1,7 @@
 // const rollup = require('rollup');
 import { OutputOptions, rollup, RollupOptions } from 'rollup';
 import babel from '@rollup/plugin-babel';
+import dts from "rollup-plugin-dts";
 import path from 'path'
 
 console.log('rollup', rollup)
@@ -9,20 +10,23 @@ const inputOptions: RollupOptions = {
   input: path.join(__dirname, "../src/lib/wx.ts"),
   plugins: [
     babel({ babelHelpers: 'bundled', extensions: ['.ts'] }),
+    dts()
   ]
 }
 
-const outputOption: OutputOptions = {
-  // file: 'bundle.js',
-  format: 'cjs',
+const outputOptions: OutputOptions[] = [{
+  format: 'es',
   dir: path.join(__dirname, "../dist")
-}
+}]
 
 build()
 async function build() {
   try {
     const bundle = await rollup(inputOptions)
-    await bundle.write(outputOption)
+    for(const outputOption of outputOptions){
+      await bundle.write(outputOption)
+    }
+    
     // const {output} = await bundle.generate(outputOption) 
     // console.log(output)
   } catch (error) {
