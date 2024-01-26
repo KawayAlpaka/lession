25965aa3ebebbce6962e11143792cb7b3e1d804f
L <my917@qq.com>
2024-01-26 14:09:30 +0800

    nextjs14 中间件示例

---
+++ nextjs/nextjs14/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // Clone the request headers and set a new header `x-hello-from-middleware1`
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-hello-from-middleware1", "hello");
+
+  // You can also set request headers in NextResponse.rewrite
+  const response = NextResponse.next({
+    request: {
+      // New request headers
+      headers: requestHeaders,
+    },
+  });
+
+  // Set a new response header `x-hello-from-middleware2`
+  response.headers.set("x-hello-from-middleware2", "hello");
+  return response;
+}
+
+// Limit the middleware to paths starting with `/api/`
+export const config = {
+  matcher: "/api/:function*",
+};
